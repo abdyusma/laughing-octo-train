898dda90b88d0cc1da3580709223f5af37049ef0
abdyusma <abdul.rahman.yusmadi@crayon.com>
2023-06-16 07:26:32 +0800

    test deploy storage and endpoint only

--- main.tf
+++ main.tf
@@ -23,32 +23,32 @@ data "azurerm_subnet" "main" {
   virtual_network_name = data.azurerm_virtual_network.main.name
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = "cmyendpoint-nic"
-  resource_group_name = data.azurerm_resource_group.main.name
-  location            = data.azurerm_resource_group.main.location
+# resource "azurerm_network_interface" "main" {
+#   name                = "cmyendpoint-nic"
+#   resource_group_name = data.azurerm_resource_group.main.name
+#   location            = data.azurerm_resource_group.main.location
 
-  ip_configuration {
-    name                          = "privateEndpointIpConfig.06e48e88-f1a7-4c7c-8604-1b07d0d8c5ba"
-    private_ip_address_allocation = "Dynamic"
-    subnet_id                     = data.azurerm_subnet.main.id
-  }
-}
+#   ip_configuration {
+#     name                          = "privateEndpointIpConfig.06e48e88-f1a7-4c7c-8604-1b07d0d8c5ba"
+#     private_ip_address_allocation = "Dynamic"
+#     subnet_id                     = data.azurerm_subnet.main.id
+#   }
+# }
 
-resource "azurerm_private_dns_zone" "main" {
-  name                = "privatelink.blob.core.windows.net"
-  resource_group_name = data.azurerm_resource_group.main.name
-  tags                = local.tags
-}
+# resource "azurerm_private_dns_zone" "main" {
+#   name                = "privatelink.blob.core.windows.net"
+#   resource_group_name = data.azurerm_resource_group.main.name
+#   tags                = local.tags
+# }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "main" {
-  name                = "6i2dd5veyofay"
-  resource_group_name = data.azurerm_resource_group.main.name
-  virtual_network_id  = data.azurerm_virtual_network.main.id
-  private_dns_zone_name = azurerm_private_dns_zone.main.name
+# resource "azurerm_private_dns_zone_virtual_network_link" "main" {
+#   name                = "6i2dd5veyofay"
+#   resource_group_name = data.azurerm_resource_group.main.name
+#   virtual_network_id  = data.azurerm_virtual_network.main.id
+#   private_dns_zone_name = azurerm_private_dns_zone.main.name
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
 resource "azurerm_storage_account" "main" {
   name                     = "cmydevops"
@@ -68,12 +68,12 @@ resource "azurerm_private_endpoint" "main" {
   location            = data.azurerm_resource_group.main.location
   subnet_id           = data.azurerm_subnet.main.id
 
-  custom_network_interface_name = azurerm_network_interface.main.name
+  # custom_network_interface_name = azurerm_network_interface.main.name
 
-  private_dns_zone_group {
-    name                 = "default"
-    private_dns_zone_ids = [azurerm_private_dns_zone.main.id]
-  }
+  # private_dns_zone_group {
+  #   name                 = "default"
+  #   private_dns_zone_ids = [azurerm_private_dns_zone.main.id]
+  # }
 
   private_service_connection {
     name                           = "cmyendpoint"
@@ -86,10 +86,10 @@ resource "azurerm_private_endpoint" "main" {
   tags = local.tags
 }
 
-resource "azurerm_private_dns_a_record" "main" {
-  name                = "cmydevops"
-  resource_group_name = data.azurerm_resource_group.main.name
-  zone_name           = azurerm_private_dns_zone.main.name
-  ttl                 = "10"
-  records             = [azurerm_network_interface.main.private_ip_address]
-}
+# resource "azurerm_private_dns_a_record" "main" {
+#   name                = "cmydevops"
+#   resource_group_name = data.azurerm_resource_group.main.name
+#   zone_name           = azurerm_private_dns_zone.main.name
+#   ttl                 = "10"
+#   records             = [azurerm_network_interface.main.private_ip_address]
+# }
